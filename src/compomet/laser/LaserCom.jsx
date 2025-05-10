64c5f03c76c7a6ed38290a40b6740987f8a1f619
MUSLIMA <muslimabintea@gmail.com>
2025-05-10 21:42:35 +0600

    HP Laser Jet Printer toner Model1-15

--- src/compomet/laser/LaserCom.jsx
+++ src/compomet/laser/LaserCom.jsx
@@ -37,6 +37,123 @@ class LaserCom extends Component {
                                 <td>1,100</td>
                                 <td>5,500</td>
                             </tr>
+                            <tr>
+                                <td>3</td>
+                                <td>HP P2011, P2011n, P2012, P2012n, P2013, P2013n, P2014, P2014n, P2014n, P2015d, P2015n, P2015dn, P2015x, M2727nf, M2727nfs, HP 1160, 1320N, 1320TN, 3390MFP, 3392MFP,  LBP-3300, 3360</td>
+                                <td>49A/53A/308/315/508/515/708</td>
+                                <td>1,000</td>
+                                <td>1,200</td>
+                                <td>1,500</td>
+                                <td>1,500</td>
+                            </tr>
+                            <tr>
+                                <td>4</td>
+                                <td>HP 1200, 1220, 3380, LBP-1210</td>
+                                <td>15A/EP25</td>
+                                <td>900</td>
+                                <td>1,200</td>
+                                <td>1,400</td>
+                                <td>Call</td>
+                            </tr>
+                            <tr>
+                                <td>5</td>
+                                <td>HP P1007, 1008, 1106, 1108, M1136, 1213nf, 1216nfh</td>
+                                <td>88A</td>
+                                <td>800</td>
+                                <td>1,000</td>
+                                <td>1,200</td>
+                                <td>Call</td>
+                            </tr>
+                            <tr>
+                                <td>6</td>
+                                <td>HP P1505, M1120, M522</td>
+                                <td>36A</td>
+                                <td>800</td>
+                                <td>1,000</td>
+                                <td>1,300</td>
+                                <td>Call</td>
+                            </tr>
+                            <tr>
+                                <td>7</td>
+                                <td>HP P1566, 1567, 1568, 1569, 1606, 1606dn</td>
+                                <td>78A/326</td>
+                                <td>700</td>
+                                <td>900</td>
+                                <td>1,100</td>
+                                <td>Call</td>
+                            </tr>
+                            <tr>
+                                <td>8</td>
+                                <td>HP  P2030, 2035, 2035n, 2050, 2055d, P2055n, 2055x, 400, M401, M401dn, M425</td>
+                                <td>05A/80A/319/719</td>
+                                <td>900</td>
+                                <td>1,100</td>
+                                <td>1,300</td>
+                                <td>12,000</td>
+                            </tr>
+                            <tr>
+                                <td>9</td>
+                                <td>HP P1005, P1006, P1100, P1102, P1102W, P1503, P1506, M1132, LBP 6000, 6030, 6200, 3018, 3010, M1214</td>
+                                <td>85A/325</td>
+                                <td>700</td>
+                                <td>900</td>
+                                <td>1,100</td>
+                                <td>8,200</td>
+                            </tr>
+                            <tr>
+                                <td>10</td>
+                                <td>HP 1300</td>
+                                <td>13A</td>
+                                <td>800</td>
+                                <td>900</td>
+                                <td>1,100</td>
+                                <td>7,500</td>
+                            </tr>
+                            <tr>
+                                <td>11</td>
+                                <td>HP P3005, M3027mfp, M3035mfp</td>
+                                <td>51A</td>
+                                <td>1,500</td>
+                                <td>1,700</td>
+                                <td>2,000</td>
+                                <td>Call</td>
+                            </tr>
+                            <tr>
+                                <td>12</td>
+                                <td>HP 2400, 2410, 2420, 243, LBP 3410, 3460</td>
+                                <td>11A/310</td>
+                                <td>1,500</td>
+                                <td>1,700</td>
+                                <td>2,000</td>
+                                <td>Call</td>
+                            </tr>
+                            <tr>
+                                <td>13</td>
+                                <td>HP P3015, 3015d, 3015dn, LBP-6750dn</td>
+                                <td>55A/324</td>
+                                <td>1,500</td>
+                                <td>1,800</td>
+                                <td>2,000</td>
+                                <td>Call</td>
+                            </tr>
+                            <tr>
+                                <td>14</td>
+                                <td>HP P4015n, 4015tn, 4015dn, 4015x, 4515n, 4515tn</td>
+                                <td>64A</td>
+                                <td>1,900</td>
+                                <td>2,200</td>
+                                <td>2,500</td>
+                                <td>Call</td>
+                            </tr>
+                            <tr>
+                                <td>15</td>
+                                <td>HP 4050, 4050n, 4050TN</td>
+                                <td>27x</td>
+                                <td>2,000</td>
+                                <td>2,500</td>
+                                <td>3,000</td>
+                                <tx>Call</tx>
+                            </tr>
                         </tbody>
                     </Table>
                 </Container>
