3922d7038ecccc265eede38c002b89d8d7794935
MUSLIMA <muslimabintea@gmail.com>
2025-06-30 15:20:08 +0600

    Photocopy Toner

--- src/compomet/photoCopy/PhotoCopy.jsx
+++ src/compomet/photoCopy/PhotoCopy.jsx
@@ -248,8 +248,250 @@ class PhotoCopy extends Component {
                                 <td>2,900</td>
                                 <td>Call</td>
                             </tr>
+                            <tr>
+                                <td>7</td>
+                                <td>CANON 2230, 2270, 2830, 2870, 3025, 3030, 3225, 3230</td>
+                                <td> NPG-25</td>
+                                <td>2,900</td>
+                                <td>Call</td>
+                            </tr>
+                            <tr>
+                                <td>8</td>
+                                <td>CANON 1607, 1610, 2000, 155, 200</td>
+                                <td>NPG-20</td>
+                                <td>1,450</td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>9</td>
+                                <td>CANON NP 7160, 7161, 7210</td>
+                                <td>NPG-15</td>
+                                <td>2,150</td>
+                                <td>Call</td>
+                            </tr>
+                            <tr>
+                                <td>10</td>
+                                <td>CANON NP 1015, 1215, 1215s, 121, 1318, 1510</td>
+                                <td>1215</td>
+                                <td>2,050</td>
+                                <td>Call</td>
+                            </tr>
+                            <tr>
+                                <td>11</td>
+                                <td>CANON IR-1435</td>
+                                <td>NPG-68</td>
+                                <td>Call</td>
+                                <td>6,500</td>
+                            </tr>
+                            <tr>
+                                <td>12</td>
+                                <td>CANON IR-2535i</td>
+                                <td>NPG-50</td>
+                                <td>Call</td>
+                                <td>4,000</td>
+                            </tr>
+                            <tr>
+                                <td>13</td>
+                                <td>CANON IR C3020, 3320, 3520i, C3525i (C3020 BK 10,000/-, Cyan 14,000/-, Magenta 14,000/-, Yellow 14,000/-)</td>
+                                <td>NPG-67</td>
+                                <td>Call</td>
+                                <td>52,000</td>
+                            </tr>
+                            <tr>
+                                <td>14</td>
+                                <td>Canon imageRunner 1643i 1643iF 1643</td>
+                                <td>T-06</td>
+                                <td>5,000</td>
+                                <td>Call</td>
+                            </tr>
+                        </tbody>
+                        <thead>
+                            <tr>
+                                <th>SL</th>
+                                <th>KONICA Photocopy Machine Model</th>
+                                <th>Toner Model</th>
+                                <th>China Toner Price</th>
+                                <th>Original Toner Price</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>TN 414</td>
+                                <td>TN-414</td>
+                                <td>2,300</td>
+                                <td>Call</td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>TN 311</td>
+                                <td>TN 311</td>
+                                <td>2,300</td>
+                                <td>Call</td>
+                            </tr>
+                            <tr>
+                                <td>3</td>
+                                <td>TM 211</td>
+                                <td>TM 211</td>
+                                <td>2,100</td>
+                                <td>Call</td>
+                            </tr>
+                            <tr>
+                                <td>4</td>
+                                <td>TN 211</td>
+                                <td>TN 118</td>
+                                <td>2,100</td>
+                                <td>Call</td>
+                            </tr>
+                            <tr>
+                                <td>5</td>
+                                <td>TN 116</td>
+                                <td>TN 116</td>
+                                <td>2,100</td>
+                                <td>Call</td>
+                            </tr>
+                            <tr>
+                                <td>6</td>
+                                <td>TN- 114</td>
+                                <td>TN 114</td>
+                                <td>2,050</td>
+                                <td>Call</td>
+                            </tr>
+                        </tbody>
+                        <thead>
+                            <tr>
+                                <th>SL</th>
+                                <th>RICOH Photocopy Machine Model</th>
+                                <th>Toner Model</th>
+                                <th>China Toner Price</th>
+                                <th>Original Toner Price</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>MP 25015</td>
+                                <td>MP-25015</td>
+                                <td>1,600</td>
+                                <td>Call</td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>MP- 2500E</td>
+                                <td>MP- 2500E</td>
+                                <td>1,800</td>
+                                <td>Call</td>
+                            </tr>
+                            <tr>
+                                <td>3</td>
+                                <td>MP-1230D</td>
+                                <td>MP-1230D</td>
+                                <td>1,400</td>
+                                <td>Call</td>
+                            </tr>
+                            <tr>
+                                <td>4</td>
+                                <td>Lanier MP- C2003 (Price: 2503S Black 6000/-, 2503S Cyan 14,500/-, 2503S Magenta 14,500/-, 2503S Yellow 14,500/-)</td>
+                                <td>MP- 2503S</td>
+                                <td>Call</td>
+                                <td>49,500 (Set)</td>
+                            </tr>
+                            <tr>
+                                <td>5</td>
+                                <td>MP 2014</td>
+                                <td>MP-2014</td>
+                                <td>1,300</td>
+                                <td>3,500</td>
+                            </tr>
+                        </tbody>
+                        <thead>
+                            <tr>
+                                <th>SL</th>
+                                <th>KYOCERA Photocopy Machine Model</th>
+                                <th>Toner Model</th>
+                                <th>China Toner Price</th>
+                                <th>Original Toner Price</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>MITA- KM-2560, 3060</td>
+                                <td>TK-679</td>
+                                <td>5,800</td>
+                                <td>Call</td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>221, 220, 1818, 180</td>
+                                <td>TK-439</td>
+                                <td>3.400</td>
+                                <td>Call</td>
+                            </tr>
+                            <tr>
+                                <td>3</td>
+                                <td>KM 1620, 1650, 2050, 2020</td>
+                                <td>TK-410</td>
+                                <td>3,100</td>
+                                <td>Call</td>
+                            </tr>
+                            <tr>
+                                <td>4</td>
+                                <td>Taskalf 1800, 1801, 2200, 2201</td>
+                                <td>TK-409</td>
+                                <td>4,000</td>
+                                <td>Call</td>
+                            </tr>
+                        </tbody>
+                        <thead>
+                            <tr>
+                                <th>SL</th>
+                                <th>SHARP Photocopy Machine Model</th>
+                                <th>Toner Model</th>
+                                <th>China Toner Price</th>
+                                <th>Original Toner Price</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+
+                                <td>1</td>
+                                <td>SF 5520, 5516, 5320, 5316</td>
+                                <td>SF 5520</td>
+                                <td>3,000</td>
+                                <td>Call</td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>2355, 1856, 1755</td>
+                                <td>SF-2355</td>
+                                <td>3,200</td>
+                                <td>Call</td>
+                            </tr>
+                            <tr>
+                               <td>3</td>
+                               <td>SF-2030</td>
+                               <td>SF-2030</td>
+                               <td>3,100</td>
+                               <td>Call</td> 
+                            </tr>
+                            <tr>
+                                <td>4</td>
+                                <td>MX-261, M261N, 311, M311N</td>
+                                <td>MX-312- AT</td>
+                                <td>3,800</td>
+                                <td>Call</td>
+                            </tr>
+                            <tr>
+                                <td>5</td>
+                                <td>Sharp SF-2030, AR-6020, AR-6023, AR-6026, AR-6031</td>
+                                <td>MX-237AT</td>
+                                <td>2,500</td>
+                                <td>7,500</td>
+                            </tr>
                         </tbody>
                     </Table>
+                    <p>Photocopy Toner সুলভ মূল্যে পাওয়া যায় ।</p>
                 </Container>
             </Fragment>
         );
